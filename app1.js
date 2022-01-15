d4c7e93cb21961d742fd7f4f59c9cfdae7bc7140
naruepol <naruapon@gmail.com>
2022-01-14 21:58:13 -0800

    Initial commit

---
+++ app1.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var handles = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.end('yes you can\n');
+}).listen(8000);
+console.log("server running port 8000");
+
+var http = require('http');
