6bc09f0e8aa88ac54e437f995c39ecccbdb9c30d
naruepol <naruapon@gmail.com>
2022-01-15 23:27:07 -0800

    Section 2-2-1 Update app.js  (add get)

--- node/projectexpress/app.js
+++ node/projectexpress/app.js
@@ -1,6 +1,11 @@
 var express = require('express');
+const { runInNewContext } = require('vm');
 var app = express();
 var server = require('http').Server(app);
 server.listen(8000);
 app.use(express.static('web'));
 console.log("Server runnning port 8000");
+
+app.get("/user", function(req, res){
+    res.json({ code : "007"});
+});
